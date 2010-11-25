7200036a1255bb06a35752fcb9dad6b4651e13e8
Eugene Russkikh <eugene@russkikh.org.ua>
2010-11-25 11:25:05 +0200

    multi_text_field_with_auto_complete - it's really cool stuff =)

--- lib/auto_complete_macros_helper.rb
+++ lib/auto_complete_macros_helper.rb
@@ -111,6 +111,20 @@ module AutoCompleteMacrosHelper
     auto_complete_field("#{object}_#{method}", { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
   end
 
+  # It works for each item, not only for first
+  # 
+  def multi_text_field_with_auto_complete(object, method, tag_options = {}, completion_options = {})
+	  if(tag_options[:index])
+	      tag_name = "#{object}_#{tag_options[:index]}_#{method}"
+	  else
+	      tag_name = "#{object}_#{method}"
+	  end
+
+	  (completion_options[:skip_style] ? "" : auto_complete_stylesheet) + text_field(object, method, tag_options) +
+    content_tag("div", "", :id => tag_name + "_auto_complete", :class => "auto_complete") +
+    auto_complete_field(tag_name, { :url => { :action => "auto_complete_for_#{object}_#{method}" } }.update(completion_options))
+  end
+
   private
     def auto_complete_stylesheet
       content_tag('style', <<-EOT, :type => Mime::CSS)
